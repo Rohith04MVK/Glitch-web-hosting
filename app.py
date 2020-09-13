8fe0b3577eb5dcd5ae42d8e15a1f65d8335de521
janaSunrise <warriordefenderz@gmail.com>
2020-09-13 16:06:56 +0530

    Clean up Python code

--- app.py
+++ app.py
@@ -1,25 +1,56 @@
 from flask import Flask, redirect, url_for, request, flash
 from flask.templating import render_template
+
 from flask_wtf import FlaskForm
+
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, ValidationError, InputRequired
+
 import requests
-toppings = ["Pepperoni", "Cheese", "Tomatoes",
-            "Brocol", "Bacon", "Chilliflakes"]
-toppings_price = {"Pepperoni": 28, "Cheese": 28,
-                  "Tomatoes": 28, "Brocol": 28, "Bacon": 28, "Chilliflakes": 0}
-price = {"Margherita": 280, "Pepperoni Feast": 290,
-         "Veggie Supreme": 340, "Ultimate chicken": 305, "Original Pan": 210}
-pizza = ["Margherita", "Pepperoni Feast",
-         "Veggie Supreme", "Ultimate chicken", "Original Pan"]
+
+toppings = [
+	"Pepperoni", 
+	"Cheese",
+	"Tomatoes",
+	"Brocol", 
+	"Bacon", 
+	"Chilliflakes"
+]
+
+toppings_price = {
+	"Pepperoni": 28, 
+	"Cheese": 28,
+	"Tomatoes": 28,
+	"Brocol": 28,
+	"Bacon": 28,
+	"Chilliflakes": 0
+}
+
+price = {
+	"Margherita": 280, 
+	"Pepperoni Feast": 290,
+	"Veggie Supreme": 340,
+	"Ultimate chicken": 305,
+	"Original Pan": 210
+}
+
+pizza = [
+	"Margherita",
+	"Pepperoni Feast",
+	"Veggie Supreme",
+	"Ultimate chicken",
+	"Original Pan"
+]
 
 
 def get_pincode():
     url = (f'https://api.postalpincode.in/pincode/{code}')
     response = requests.get(url, headers={"Accept": "application/json"})
     data = response.json()
+    
     global Name
     Name = (data[0]['PostOffice'][0]['Name'])
+    
     return Name
 
 
@@ -66,10 +97,13 @@ def welcome():
 def menu():
     form = pizza_form()
     pizzas = price
+    
     global piz
     piz = form.pizza.data
+    
     if form.validate_on_submit():
         return redirect(url_for('order'))
+
     return render_template('menu.html', form=form, pizzas=pizzas)
 
 
@@ -77,9 +111,12 @@ def menu():
 def order():
     global top # scope of this variable is global
     toppin = toppings_price
+
     top = (request.form.getlist('checkbox1'))
+    
     if request.method == 'POST' and len(top) <= 4:
         return redirect(url_for('done'))
+
     return render_template('order.html', toppin=toppin)
 
 
@@ -87,6 +124,7 @@ def order():
 def done():
     form = bill()
     place = Name
+
     return render_template("done.html", form=form, place=place)
 
 
@@ -95,8 +133,10 @@ def bill():
     for r in top:
         f = toppings_price.get(r)
         toppings_price_list.append(f)
+
     toppings_total = sum(toppings_price_list)
     bill = (int(price.get(piz))) + (toppings_total)
+
     return(str(bill) + " ₹")
 
 
